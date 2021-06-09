97fb5c858c056024c79cbdfdb3e1c940b3ee4bf7
EC2 Default User <ec2-user@ip-172-31-94-194.ec2.internal>
2021-06-09 11:45:47 +0000

    login signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'posts#index'
+
+  devise_for :users,
+    path: '',
+    path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
+    controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
+
+  # get '/users/:id', to: 'users#show'
+  # /users/3 -> Users controller, show action, params {id: '3'}
+  resources :users, only: [:index, :show]
+
 end
